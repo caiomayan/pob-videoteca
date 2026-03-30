1fb6db0e5002c5a3036772728ba6379d95078e35
Messias Teixeira <messias4910@gmail.com>
2026-03-30 09:13:36 -0300

    refactor/corrigindo lógica de remoção

--- src/appconsole/Apagar.java
+++ src/appconsole/Apagar.java
@@ -1,6 +1,7 @@
 package appconsole;
 
 import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.db4o.ObjectContainer;
@@ -16,89 +17,36 @@ public class Apagar {
     public Apagar() {
         Util.conectar();
         manager = Util.getManager();
-    }
+  
 
-    public void apagarTudo() {
-        Query q = manager.query();
-
-        System.out.println("\nApagando vídeos...");
-        q.constrain(Video.class);
-        List<Video> listaVideos = q.execute();
-        for (Video video : listaVideos) {
-            manager.delete(video);
-        }
-
-        System.out.println("Apagando gêneros...");
-        q = manager.query();
-        q.constrain(Genero.class);
-        List<Genero> listaGeneros = q.execute();
-        for (Genero genero : listaGeneros) {
-            manager.delete(genero);
-        }
-
-        manager.commit(); 
-    }
-
-    public void apagarVideo(int id) {
         Query q = manager.query();
         q.constrain(Video.class);
-        q.descend("id").constrain(id);
+        q.descend("classificacao").constrain("Livre"); // Remover primeiro video com classificacao 14
 
         List<Video> lista = q.execute();
 
         if (lista.isEmpty()) {
-            System.out.println("Vídeo não encontrado.");
+            System.out.println("Não existe vídeos com classificação 14.");
         } else {
         	Video video = lista.get(0);
-        	if (video.getListaGeneros() != null) {
-	        	for (Genero g : new ArrayList<>(video.getListaGeneros())) {
-	        		if (g.getListaVideos().contains(video)) {
-	        			g.removerVideo(video);
-	        			manager.store(g);
-	        		}
-	        	}
-        	}
+        	for (Genero g : new ArrayList<>(video.getListaGeneros())) {
+        		if (g.getListaVideos().contains(video)) {
+        			g.removerVideo(video);
+        			manager.store(g);
+        		}
+	        }
+        	
             manager.delete(lista.get(0));
             manager.commit();
-            System.out.println("Vídeo removido.");
+            System.out.println("Vídeo '" + video.getTitulo() + "' Removido");
         }
-    }
-
-    public void apagarGenero(int id) {
-        Query q = manager.query();
-        q.constrain(Genero.class);
-        q.descend("id").constrain(id);
-
-        List<Genero> lista = q.execute();
-
-        if (lista.isEmpty()) {
-            System.out.println("Gênero não encontrado.");
-        } else {
-        	Genero genero = lista.get(0);
-        	if (genero.getListaVideos() != null) {
-	        	for (Video v : new ArrayList<>(genero.getListaVideos())) {
-	        		if (v.getListaGeneros().contains(genero)) {
-		        		v.removerGenero(genero);
-		        		manager.store(v);
-	        		}
-	        	}
-        	}
-            manager.delete(lista.get(0));
-            manager.commit();
-            System.out.println("Gênero removido.");
-        }
-    }
-
-    public void fechar() {
+ 
         Util.desconectar();
     }
 
-    public static void main(String[] args) {
-        Apagar ap = new Apagar();
-        
-        ap.apagarVideo(70);
-        ap.apagarGenero(57);
 
-        ap.fechar();
-    }
+public static void main(String[] args) {
+    	new Apagar();
+
+	}
 }
